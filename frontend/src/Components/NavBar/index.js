c2b5403f219157297310c27ade0968799961f68e
TerribileDev <vitor.terribile.anjos@gmail.com>
2024-05-02 14:00:22 -0300

    Navbar

--- frontend/src/Components/NavBar/index.js
+++ frontend/src/Components/NavBar/index.js
@@ -1,28 +1,30 @@
 import { Box } from '@chakra-ui/react';
 import { Image } from '@chakra-ui/react';
-import { Avatar, Stack, Input } from '@chakra-ui/react'
+import { Avatar, Stack, Input, Flex } from '@chakra-ui/react'
 
 
 const NavBar = () => {
   return <>
     <Box w='100%' h={115} bg='darkblue'>
-      <Stack direction='col' spacing={10}>
+      <center>
+        <Stack direction='row' justify='center' spacing={5}>
 
+          <Image w={150} align="center" src='https://www.moveisbartira.com.br/images/logos/bartira-branco-vermelho.svg?256&q=75' alt='Bartira' />
 
+          <Input borderRadius={20} bg='white' m={35} w={500} justify="center" align="center" placeholder='O que você está buscando?' />
 
-        <Image w={150} ml={18} align="center" src='https://www.moveisbartira.com.br/images/logos/bartira-branco-vermelho.svg?256&q=75' alt='Bartira' />
+          <Avatar mt={30} src='https://bit.ly/broken-link' />
+          <Avatar mt={30} src='https://bit.ly/broken-link' />
+          <Avatar mt={30} src='https://bit.ly/broken-link' />
 
-        
-        <Input borderRadius={20} bg='white' m={35} w={400} justify="center" align="center" placeholder='O que você está buscando?'/>
-
-        <Avatar mt={30} ml={50} name='Oshigaki Kisame' src='https://bit.ly/broken-link' />
-        <Avatar mt={30} ml={50} name='Sasuke Uchiha' src='https://bit.ly/broken-link' />
-        <Avatar mt={30} ml={50} src='https://bit.ly/broken-link' />
-
-      </Stack>
+        </Stack>
+      </center>
     </Box>
+
 
   </>
 }
 
 export default NavBar;
+
+
