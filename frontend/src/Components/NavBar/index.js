a6719d20384a703307f7f2b93fc994a2428e202c
TerribileDev <vitor.terribile.anjos@gmail.com>
2024-05-02 14:03:41 -0300

    Navbar

--- frontend/src/Components/NavBar/index.js
+++ frontend/src/Components/NavBar/index.js
@@ -6,7 +6,6 @@ import { Avatar, Stack, Input, Flex } from '@chakra-ui/react'
 const NavBar = () => {
   return <>
     <Box w='100%' h={115} bg='darkblue'>
-      <center>
         <Stack direction='row' justify='center' spacing={5}>
 
           <Image w={150} align="center" src='https://www.moveisbartira.com.br/images/logos/bartira-branco-vermelho.svg?256&q=75' alt='Bartira' />
@@ -18,7 +17,6 @@ const NavBar = () => {
           <Avatar mt={30} src='https://bit.ly/broken-link' />
 
         </Stack>
-      </center>
     </Box>
 
 
