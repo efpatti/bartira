da8990019ebdc49b4a9ddad2b2a5f3b831e62061
efpatti <enzo.patti@aluno.senai.br>
2024-05-07 09:59:20 -0300

    alterado estrutura para testes (app1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import NavBar from "./Components/NavBar"
+import NavBar from "./Components/NavBar";
+import DashBoard from "./Pages/DashBoard";
+import Funcionarios from "./Pages/Funcionarios";
+import Home from "./Pages/Home";
+import Vendas from "./Pages/Vendas";
+import Financeiro from "./Pages/Financeiro";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/funcionarios" element={<Funcionarios />} />
+          <Route path="/vendas" element={<Vendas />} />
+          <Route path="/financeiro" element={<Financeiro />} />
+          <Route path="/produtos" element={<DashBoard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
