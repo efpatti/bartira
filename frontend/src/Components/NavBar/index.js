f07622a476f6b057b200b9f3d060bf963a4d7862
efpatti <enzo.patti@aluno.senai.br>
2024-05-07 10:15:08 -0300

    dark mode navbar atualizado

--- frontend/src/Components/NavBar/index.js
+++ frontend/src/Components/NavBar/index.js
@@ -1,21 +1,24 @@
-import { Box, Flex, Input, IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Input, IconButton, useColorMode } from "@chakra-ui/react";
 import { SearchIcon, MoonIcon, SunIcon, SettingsIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
-  const { toggleColorMode } = useColorMode();
-  const iconColor = useColorModeValue("white", "blue");
+  const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <Box bg={"darkblue"} p={4}>
       <Flex alignItems="center">
         {/* Logo */}
         <Box>
-          <img src="https://www.moveisbartira.com.br/images/logos/bartira-branco-vermelho.svg?256&q=75" alt="bartira" width="150" ml={50} />
+          <img
+            src="https://www.moveisbartira.com.br/images/logos/bartira-branco-vermelho.svg?256&q=75"
+            alt="bartira"
+            width="150"
+            ml={50}
+          />
         </Box>
-        
+
         {/* Barra de Pesquisa */}
         <Input
-          
           w={500}
           align="center"
           ml="21%"
@@ -29,22 +32,31 @@ const NavBar = () => {
           <IconButton
             arial-label="Login"
             icon={<SettingsIcon />}
-            color={iconColor}
+            color={colorMode === "light" ? "white" : "black"}
             bg={0}
+            _hover={{
+              color: colorMode === "light" ? "gray.600" : "gray.300",
+            }}
           />
           <IconButton
             aria-label="Pesquisar"
             icon={<SearchIcon />}
-            color={iconColor}
+            color={colorMode === "light" ? "white" : "black"}
             bg={0}
+            _hover={{
+              color: colorMode === "light" ? "gray.600" : "gray.300",
+            }}
           />
           <IconButton
             ml={4}
             aria-label="Alternar modo de cor"
-            icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+            icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
             onClick={toggleColorMode}
-            color={iconColor}
+            color={colorMode === "light" ? "white" : "black"}
             bg={0}
+            _hover={{
+              color: colorMode === "light" ? "gray.600" : "gray.300",
+            }}
           />
         </Flex>
       </Flex>
@@ -53,5 +65,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-
